97bab3e5e94595954ad4af9a53ba78ccdbc8a87e
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-05-23 10:11:23 +0600

    minor changes

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -3,10 +3,10 @@ import './SignIn.scss';
 import { useAuth0 } from "@auth0/auth0-react";
 
 const SignIn = () => {
-    const { loginWithPopup, logout } = useAuth0()
+    const { loginWithRedirect, logout } = useAuth0()
     return (
         <div className='signInCard'>
-            <button onClick={() => loginWithPopup()} className='btnSignIn'>Sing In</button>
+            <button onClick={() => loginWithRedirect()} className='btnSignIn'>Sing In</button>
             <button onClick={() => logout()} className='btnSignIn'>Sing Out</button>
         </div>
     );
